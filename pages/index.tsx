b67e42c12abcc1977916db55a7ac5804629978ef
asahi <ojimaasahi1018@gmail.com>
2023-05-17 21:09:34 +0900

    ✨評価フォームの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { Inter, Big_Shoulders_Display } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const big_Shoulders_Display = Big_Shoulders_Display({ subsets: ["latin"] });
 
 import {
   Select,
@@ -8,41 +12,81 @@ import {
   Button,
   createStyles,
   Textarea,
+  Header,
 } from "@mantine/core";
 
 const useStyles = createStyles((theme) => ({
-  main: {
-    margin: "30px auto",
+  header: {
+    padding: "0 20px",
+    display: "flex",
+    position: "fixed",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon_container: {
+    position: "absolute",
+    zIndex: 200,
+    display: "flex",
+    alignItems: "center",
+    top: 20,
+    left: 40,
+  },
+
+  main_container: {
+    margin: "95px 20px 30px",
   },
   input_wrapper: {
-    margin: "20px 0",
+    margin: "20px 0 60px",
   },
   input: {
-    display: "inline-block",
-    width: "100%",
+    marginTop: "15px",
   },
 }));
 
 export default function Home() {
   const { classes } = useStyles();
   return (
-    <Box className={styles.container}>
+    <Box>
       <Head>
         <title>Lectureview</title>
         <meta
           name="description"
           content="東京理科大学の講義をレビューするサイトです。"
         />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={classes.main}>
+      <Header className={classes.header} height={65}>
+        <Box className={classes.icon_container}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#00abfb"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+            <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+          </svg>
+        </Box>
+        <h1 className={big_Shoulders_Display.className}>Lectureview</h1>
+      </Header>
+      <main className={classes.main_container}>
         <form action="/review" className="label" method="POST">
           <Input.Wrapper className={classes.input_wrapper}>
             <Input.Label htmlFor="lecture" size="xl">
               レビューする講義
             </Input.Label>
-            <Input name="lecture" id="lecture"></Input>
+            <Input
+              className={classes.input}
+              name="lecture"
+              id="lecture"
+            ></Input>
           </Input.Wrapper>
 
           <Input.Wrapper className={classes.input_wrapper}>
@@ -50,6 +94,7 @@ export default function Home() {
               評価
             </Input.Label>
             <Select
+              className={classes.input}
               id="score"
               name="score"
               data={[
@@ -60,16 +105,16 @@ export default function Home() {
                 { value: "5", label: "5" },
               ]}
             ></Select>
-            <Input type="s" id="score" name="score"></Input>
           </Input.Wrapper>
           <Input.Wrapper className={classes.input_wrapper}>
             <Input.Label htmlFor="comment" size="xl">
               コメント
             </Input.Label>
             <Textarea
+              className={classes.input}
               name="comment"
               id="comment"
-              placeholder="Autosize with no rows limit"
+              placeholder="太郎先生は頭が悪い。"
               autosize
               minRows={10}
             />
